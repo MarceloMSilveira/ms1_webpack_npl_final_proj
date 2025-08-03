f116797ccc211af966208b15fb80cd4b9c9e7f48
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-08-03 10:11:04 -0300

    jest test done

--- frontend/scripts/setReset.js
+++ frontend/scripts/setReset.js
@@ -1,4 +1,22 @@
 export default function setReset(analizingText, divResult, inputUrl) {
+
+  const args = {analizingText, divResult, inputUrl};
+  //verificando se tem argumento que não é um elemento html ou não está no DOM
+  for(const [name,el] of Object.entries(args)) {
+    for (const [name, el] of Object.entries(args)) {
+      if (el === null || el === undefined) {
+        throw new Error('Elemento(s) nulo(s) passado(s) para a função.');
+      }
+      if (!(el instanceof HTMLElement)) {
+        throw new Error(`${name} não é um elemento HTML válido.`);
+      }
+      if (!document.body.contains(el)) {
+        throw new Error(`${name} não está presente no DOM.`);
+      }
+    }
+  }
+  
+  
   analizingText.style.display='none';
   divResult.innerHTML='';
   inputUrl.placeholder='url to parse';
---
+++ frontend/scripts/setReset.test.js
@@ -0,0 +1,48 @@
+import setReset from "./setReset.js";
+//Elemento(s) nulo(s) passado(s) para a função.
+beforeEach(()=>{
+  document.body.innerHTML = `
+    <input type="text" id="inputUrl" name="" value="">
+    <p id="analizingText"></p>
+    <div id="divResult">
+      
+    </div>
+  `
+});
+
+test('executa com sucesso e limpa a interface',()=>{
+  const analizingText = document.querySelector('#analizingText');
+  const divResult = document.querySelector('#divResult');
+  const inputUrl = document.querySelector('#inputUrl');
+
+  // conteúdo prévio
+    divResult.innerHTML = '<p>texto antigo</p>';
+    analizingText.style.display = 'block';
+    inputUrl.placeholder = 'algo antigo';
+
+  setReset(analizingText, divResult, inputUrl);
+
+  expect(divResult.innerHTML).toBe('');
+  expect(analizingText.style.display).toBe('none');
+  expect(inputUrl.placeholder).toBe('url to parse');
+});
+
+test('Lança erro em caso de argumento nulo',()=>{
+  const analizingText = document.querySelector('#analizingText');
+  const inputUrl = document.querySelector('#inputUrl');
+  const divResult = null; //argumento nulo
+
+  expect(()=>setReset(analizingText, divResult, inputUrl).toThrow(/nulo/));
+
+});
+
+test('lança erro se o elemento não estiver no DOM', () => {
+  const analizingText = document.createElement('p'); // não inserido no DOM
+  const divResult = document.querySelector('#divResult');
+  const inputUrl = document.querySelector('#inputUrl');
+
+  expect(() => {
+    setReset(analizingText, divResult, inputUrl);
+  }).toThrow('analizingText não está presente no DOM.');
+});
+
