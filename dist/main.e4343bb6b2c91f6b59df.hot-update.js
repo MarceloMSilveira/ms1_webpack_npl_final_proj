60b37d8ea03abd54a915f76789cae2d3ffdc7a87
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-08-04 08:26:25 -0300

    adjusts3

---
+++ dist/main.e4343bb6b2c91f6b59df.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdatems1_webpack_proj"]("main",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("489344a1d702c106a282")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=main.e4343bb6b2c91f6b59df.hot-update.js.map
--- src/index.js
+++ src/index.js
@@ -5,13 +5,26 @@ import "../frontend/scripts/index.js"
 
 // export {callApi, checkName};
 
- if ('serviceWorker' in navigator) {
-   window.addEventListener('load', () => {
-     navigator.serviceWorker.register('/service-worker.js').then(registration => {
-       console.log('SW registered: ', registration);
-     }).catch(registrationError => {
-       console.log('SW registration failed: ', registrationError);
-     });
-   });
- }
+//  if ('serviceWorker' in navigator) {
+//    window.addEventListener('load', () => {
+//      navigator.serviceWorker.register('/service-worker.js').then(registration => {
+//        console.log('SW registered: ', registration);
+//      }).catch(registrationError => {
+//        console.log('SW registration failed: ', registrationError);
+//      });
+//    });
+//  }
+
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for (const registration of registrations) {
+      registration.unregister().then(success => {
+        if (success) {
+          console.log('✅ Service Worker removido:', registration);
+        }
+      });
+    }
+  });
+}
 
